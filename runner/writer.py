7c4f111065a7ff7b018711fdb93270e7fc9003b6
Andrei Markin <amarkin@google.com>
2021-12-02 17:37:28 +0300

    Add dataset creation in BQ if not exists
    * Added typing and docstrings to functions
    
    Change-Id: Iec5d2afe0bed29bf698945b4e73461518f4d4070

--- runner/writer.py
+++ runner/writer.py
@@ -12,13 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.import csv
 
-from google.cloud import bigquery
-from google.cloud.exceptions import NotFound
+from typing import Sequence, Any
+from google.cloud import bigquery  #type: ignore
+from google.cloud.exceptions import NotFound  #type: ignore
 import csv
 import time
+import pandas as pd  #type: ignore
 
 
-def save_to_csv(results, column_names, destination):
+def save_to_csv(results: Sequence[Sequence[Any]], column_names: Sequence[str],
+                destination: str) -> None:
+    """Saves output of data fetching to a local csv file.
+
+    Args:
+        results: Rows without headers to be written to CSV
+        column_names: List containing names of column to be considers as
+            header of CSV
+        destination: location of file
+    Returns:
+        None
+    """
+
     with open(destination, "w") as file:
         writer = csv.writer(file,
                             delimiter=",",
@@ -28,19 +42,45 @@ def save_to_csv(results, column_names, destination):
         writer.writerows(results)
 
 
-def save_to_bq(client, project, dataset, table, results, schema):
-    destination = f"{project}.{dataset}.{table}"
+def save_to_bq(client: bigquery.Client,
+               project: str,
+               dataset: str,
+               table: str,
+               results: pd.DataFrame,
+               schema: Sequence[bigquery.SchemaField],
+               location: str = "US") -> None:
+    """Saves output of data fetching to BigQuery.
+
+    Args:
+        client: BigQuery client
+        project: BigQuery project_id
+        dataset: BigQuery dataset_id
+        table: BigQuery table name
+        results: dataframe containing data to be written to BigQuery
+        schema: BigQuery schema for results
+        location: location of dataset_id (if dataset does not exist and should
+            be created)
+
+    Returns:
+        None
+    """
+    dataset_id = f"{project}.{dataset}"
+    destination = f"{dataset_id}.{table}"
+    try:
+        client.get_dataset(dataset_id)
+    except NotFound:
+        bq_dataset = bigquery.Dataset(dataset_id)
+        bq_dataset.location = location
+        bq_dataset = client.create_dataset(bq_dataset, timeout=30)
     job_config = bigquery.LoadJobConfig(
         write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-        schema=schema
-    )
+        schema=schema)
     try:
         table = client.get_table(destination)
     except NotFound:
         table_ref = bigquery.Table(destination, schema=schema)
         table = client.create_table(table_ref)
         table = client.get_table(destination)
-    client.load_table_from_dataframe(
-        dataframe = results,
-        destination = table,
-        job_config = job_config)
+    client.load_table_from_dataframe(dataframe=results,
+                                     destination=table,
+                                     job_config=job_config)
