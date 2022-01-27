254e14373f26ad011d8507e5dc37c0341ebc1370
Andrei Markin <amarkin@google.com>
2022-01-28 02:11:25 +0300

    Update google_ads queries with missing dimensions and correct WHERE statements
    Change-Id: I0c72725bb5a80db69b9adca4e645f756034ceb0d

--- google_ads_queries/settings/ad_matching.sql
+++ google_ads_queries/settings/ad_matching.sql
@@ -1,22 +1,24 @@
 SELECT
     campaign.id AS campaign_id,
     campaign.name AS campaign_name,
+    campaign.bidding_strategy_type AS bidding_strategy,
     ad_group.name AS ad_group_name,
     ad_group_ad.ad.id AS ad_id,
+    ad_group_ad.ad.type AS ad_group_ad_type,
     ad_group_ad.ad.name AS ad_name,
     video.id AS youtube_video_id,
     video.title AS youtube_title,
     video.duration_millis AS youtube_video_duration
 FROM video
 WHERE ad_group_ad.ad.type IN (
-	"VIDEO",
-	"VIDEO_RESPONSIVE",
-	"VIDEO_BUMPER",
-	"VIDEO_NON_SKIPPABLE_IN_STREAM",
-	"VIDEO_OUTSTREAM",
-	"VIDEO_RESPONSIVE",
-	"VIDEO_TRUE_VIEW_IN_DISPLAY",
-	"VIDEO_TRUE_VIEW_IN_STREAM"
+	"VIDEO_AD",
+	"VIDEO_RESPONSIVE_AD",
+	"VIDEO_BUMPER_AD",
+	"VIDEO_NON_SKIPPABLE_IN_STREAM_AD",
+	"VIDEO_OUTSTREAM_AD",
+	"VIDEO_RESPONSIVE_AD",
+	"VIDEO_TRUEVIEW_DISCOVERY_AD",
+	"VIDEO_TRUEVIEW_IN_STREAM_AD"
 	)
     AND campaign.bidding_strategy_type IN (
 	"MAXIMIZE_CONVERSIONS",
