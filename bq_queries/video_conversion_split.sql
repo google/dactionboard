74efa85f71a06d2f79b75f07403985392561db21
Andrei Markin <amarkin@google.com>
2022-06-29 00:11:43 +0400

    Remove reference to media in video_conversion_split
    Change-Id: I77d9eacfd1a8e5364344f47c11c6389cfed31c67

--- bq_queries/video_conversion_split.sql
+++ bq_queries/video_conversion_split.sql
@@ -14,7 +14,7 @@ SELECT
     AP.ad_id,
     AM.youtube_video_id,
     AM.youtube_title AS video_title,
-    ROUND(Media.youtube_video_duration / 1000) AS video_duration,
+    ROUND(AM.youtube_video_duration / 1000) AS video_duration,
     AP.conversion_name,
     SUM(AP.all_conversions) AS all_conversions,
     SUM(AP.conversions) AS conversions,
@@ -24,6 +24,4 @@ INNER JOIN {bq_project}.{bq_dataset}.mapping AS M
   ON AP.ad_group_id = M.ad_group_id
 INNER JOIN {bq_project}.{bq_dataset}.ad_matching AS AM
   ON AP.ad_id = AM.ad_id
-LEFT JOIN {bq_project}.{bq_dataset}.media AS Media
-    ON AM.youtube_video_id = Media.youtube_video_id
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
