69b8f7f6c8e80b45e6dbf2fe968fafc84c8082c8
Andrei Markin <amarkin@google.com>
2022-07-13 21:10:52 +0400

    Add cost metric to video_conversion_split
    Change-Id: I0944b5f0f753151fca02ba670b855e073f501906

--- bq_queries/video_conversion_split.sql
+++ bq_queries/video_conversion_split.sql
@@ -1,5 +1,14 @@
 CREATE OR REPLACE TABLE {bq_project}.{bq_dataset}.video_conversion_split_F
 AS (
+WITH PerformanceTable AS (
+    SELECT
+        date,
+        ad_group_id,
+        ad_id,
+        SUM(cost) AS cost
+    FROM {bq_project}.{bq_dataset}.ad_performance
+    GROUP BY 1, 2, 3
+)
 SELECT
     PARSE_DATE("%Y-%m-%d", AP.date) AS day,
     M.account_id,
@@ -16,10 +25,13 @@ SELECT
     AM.youtube_title AS video_title,
     ROUND(AM.youtube_video_duration / 1000) AS video_duration,
     AP.conversion_name,
+    SUM(P.cost / 1e6) AS cost,
     SUM(AP.all_conversions) AS all_conversions,
     SUM(AP.conversions) AS conversions,
     SUM(AP.view_through_conversions) AS view_through_conversions
 FROM {bq_project}.{bq_dataset}.conversion_split AS AP
+INNER JOIN PerformanceTable AS P
+    USING(ad_group_id, date, ad_id)
 INNER JOIN {bq_project}.{bq_dataset}.mapping AS M
   ON AP.ad_group_id = M.ad_group_id
 INNER JOIN {bq_project}.{bq_dataset}.ad_matching AS AM
