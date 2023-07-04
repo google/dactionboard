695f118128d59112ab95ab6e67e049f693edfcea
Andrei Markin <amarkin@google.com>
2023-07-04 10:49:26 +0400

    Enclose output tables names in backticks
    Change-Id: Iac28fb18a24d1c38eff568b1300cb30fc8b7a01d

--- bq_queries/conversion_split.sql
+++ bq_queries/conversion_split.sql
@@ -11,7 +11,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-CREATE OR REPLACE TABLE {output_dataset}.conversion_split
+CREATE OR REPLACE TABLE `{output_dataset}.conversion_split`
 AS (
 SELECT
     PARSE_DATE("%Y-%m-%d", AP.date) AS day,
--- bq_queries/creative_excellence.sql
+++ bq_queries/creative_excellence.sql
@@ -11,7 +11,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-CREATE OR REPLACE TABLE {output_dataset}.creative_excellence
+CREATE OR REPLACE TABLE `{output_dataset}.creative_excellence`
 AS (
 WITH
     /* Creatives block */
